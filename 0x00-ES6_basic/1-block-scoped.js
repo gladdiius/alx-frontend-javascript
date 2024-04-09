499393dad24fca357d4dc1fc1ce0fab3b2b97e40
elias <eliaseshetu28@gmail.com>
2024-04-09 11:06:45 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,8 @@
 export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
+  // eslint-disable-next-line
     var task = true;
+  // eslint-disable-next-line
     var task2 = false;
   }
   task = false;
