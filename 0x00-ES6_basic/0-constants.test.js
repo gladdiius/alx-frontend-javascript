5d49cbd9fb2aa23543c452e2e15691e71ece10cc
elias <eliaseshetu28@gmail.com>
2024-04-09 08:10:49 +0300

    n

--- 0x00-ES6_basic/0-constants.test.js
+++ 0x00-ES6_basic/0-constants.test.js
@@ -1,4 +1,4 @@
 import { taskFirst, taskNext } from './0-constants.js';
-test('', () => {
+test(' ', () => {
     expect(`${taskFirst()} ${taskNext()}`).toBe('I prefer const when I can. But sometimes let is okay');
 });
