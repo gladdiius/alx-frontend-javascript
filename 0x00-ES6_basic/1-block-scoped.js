18f68fafbae5b0c46a0072cb8474bd697c9855d3
elias <eliaseshetu28@gmail.com>
2024-04-09 07:49:26 +0300

    updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    if (trueOrFalse) {
-      task = true;
-      task2 = false;
-    }
-    var task = false;
-    var task2 = true;
-  
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
   }
-  
+  task = false;
+  task2 = true;
+  return [task, task2];
+}
