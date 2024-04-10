a7fe51758c04f0f848d4047c46d7b562b168a545
elias <eliaseshetu28@gmail.com>
2024-04-10 22:26:01 +0300

    8-try.js

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+// Function to divide numerator by denominator
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
