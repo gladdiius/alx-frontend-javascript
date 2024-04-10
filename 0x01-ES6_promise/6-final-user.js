7150cf7cdba5a56878ec0a7d70164b11c5e85d79
elias <eliaseshetu28@gmail.com>
2024-04-10 22:12:26 +0300

    6-final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,14 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   // Call signUpUser and uploadPhoto functions with the provided arguments
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
-
   // Wait for all promises to settle using Promise.allSettled
   return Promise.allSettled([userPromise, photoPromise])
-    .then(results => {
-      // Map over the settled promises and create an array with the status and value/error
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-    });
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
